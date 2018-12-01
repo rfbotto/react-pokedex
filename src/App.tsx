bc70fa31e2f79e5d1cbc38307450c8ec54b8dacb
Rui Botto <rui.figueira@fe.up.pt>
2018-12-01 18:19:07 +0100

    Initial commit of create-react-app boilerplate with typescript

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          <span role="img" aria-label="React Christmas Advent">
+            ✡️🎄✌
+          </span>
+          React Christmas Advent
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default App;

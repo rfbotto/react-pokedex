2cdd33d6e3a279a08cbd78fa33d76812b85f50ff
Rui Botto <rui.figueira@fe.up.pt>
2018-12-09 17:08:21 +0100

    Refactor to react-cache 2.2.0-alpha.1

--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -1,17 +1,15 @@
 import React from 'react'
-// @ts-ignore react-cache types are outdated
-import { createCache, createResource } from "react-cache";
+import { unstable_createResource } from "react-cache";
 import { getPokemonCollection } from '../api/getPokemonCollection';
 import { Pokemon } from '../api';
 
-const PokemonCollectionResource = createResource(getPokemonCollection)
-const cache = createCache()
+const PokemonCollectionResource = unstable_createResource(getPokemonCollection)
 
 const PokemonList: React.SFC = () => (
     <>
         <h2>Pokemon Collection</h2>
         <ul>
-            {PokemonCollectionResource.read(cache).results.map(
+            {PokemonCollectionResource.read('pokemons').results.map(
                 (pokemon: Pokemon) => (
                     <li key={pokemon.name}>{pokemon.name}</li>
                 )
